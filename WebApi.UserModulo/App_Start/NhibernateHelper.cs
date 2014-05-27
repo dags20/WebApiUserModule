1359396f00b9be6b86b85b1eaca0bd241927e3c6
Diego Gonzalez <diego.ags20@hotmail.com>
2014-05-27 23:04:40 +0100

    Initial commit

---
+++ WebApi.UserModulo/App_Start/NhibernateHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NHibernate.AspNet.Identity;
+using NHibernate.AspNet.Identity.Helpers;
+using WebApi.UserModulo.Models;
+using NHibernate.Cfg;
+using NHibernate;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
+using System.Collections.Generic;
+
+namespace WebApi.UserModulo
+{
+    public class NhibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        public static ISession GetSession() 
+        {
+            return _sessionFactory.OpenSession();        
+        }
+
+        public static void Configure()
+        {
+            if (_sessionFactory == null) {
+
+                var configuration = new Configuration();
+                configuration.Configure();
+
+                _sessionFactory = configuration.BuildSessionFactory();
+            }
+        }
+    }
+}
---
+++ WebApi.UserModulo/Models/IdentityModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.AspNet.Identity;
+
+namespace WebApi.UserModulo.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
