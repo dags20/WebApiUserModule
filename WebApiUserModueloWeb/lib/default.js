fc71d955dd9e3dd06e6fb403873132d6569ceeb9
Diego Gonzalez <diego.ags20@hotmail.com>
2014-06-09 21:47:11 +0100

    This is the we project that will make use of the web module,

---
+++ WebApiUserModueloWeb/lib/default.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    window.addEventListener("load", function load(event) {
+        window.removeEventListener("load", load, false);
+        init();
+    }, false);
+
+    function init() {
+        document.getElementById("link").addEventListener("click", showAlert, false);
+    }
+
+    function showAlert() {
+        alert("Welcome to Pure HTML!");
+    }
+}());
